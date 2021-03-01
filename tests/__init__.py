737b7ce45ffb6d9f1227af90c96f8ea0374cd3c8
Suganya <suganprakash0621@gmail.com>
2021-03-01 14:41:27 -0600

    added tests folder and files init

---
+++ tests/__init__.py
@@ -0,0 +1,5 @@
+from .event_tests import EventTests
+from .eventgamer_tests import EventgamerTests
+from .game_tests import GameTests
+from .gamer_tests import GamerTests
+from .gametype_tests import GametypeTests
