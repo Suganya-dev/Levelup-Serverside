c8cb10d94b8f5f1c6520402adc8c27be51c2aeae
Suganya <suganprakash0621@gmail.com>
2021-03-01 16:10:38 -0600

    integration testing

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,5 +1,7 @@
-from .event_tests import EventTests
-from .eventgamer_tests import EventgamerTests
+
 from .game_tests import GameTests
-from .gamer_tests import GamerTests
-from .gametype_tests import GametypeTests
+
+# from .event_tests import EventTests
+# from .eventgamer_tests import EventgamerTests
+# from .gamer_tests import GamerTests
+# from .gametype_tests import GametypeTests
