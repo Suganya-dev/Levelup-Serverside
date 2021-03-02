b6d4d0fec2159f8d0a123fb198720a9f613e1370
Suganya <suganprakash0621@gmail.com>
2021-03-02 10:17:05 -0600

    coded for events

--- levelupapi/views/game.py
+++ levelupapi/views/game.py
@@ -68,6 +68,9 @@ class GamesViewSet(ViewSet):
             game = Games.objects.get(pk=pk)
             serializer = GamesSerializer(game, context={'request': request})
             return Response(serializer.data)
+
+        except Games.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
         except Exception as ex:
             return HttpResponseServerError(ex)
 
